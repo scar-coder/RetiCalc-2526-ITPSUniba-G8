c484da25666a1515ddb922d2d295daca89fe1362
scar-coder <scar-coder@hotmail.com>
2025-12-17 20:00:31 +0100

    creato codice di prova

--- TCP/client_TCP_G8/client.c
+++ TCP/client_TCP_G8/client.c
@@ -6,3 +6,20 @@
  */
 
 
+#include "client.h"
+int main() {
+
+	WSADATA wsaData;
+	WORD wVersionRequested;
+	wVersionRequested= MAKEWORD(2,2);
+	int iResult = WSAStartup(wVersionRequested, &wsaData);
+	if (iResult != 0) {
+		printf("Error at WSAStartup()\n");
+		printf("A usable WinSock DLL cannot be found");
+		return-1;
+	}
+	// The WinSock DLL is acceptable. Proceed
+	printf("No errors occurred. \n");
+	system("pause");
+	return 0;
+}
